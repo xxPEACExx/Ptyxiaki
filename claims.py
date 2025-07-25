9abdb5f58c6a60a893ad8df1f2724daf639ebae9
xxPEACExx <73955875+xxPEACExx@users.noreply.github.com>
2025-07-25 11:48:35 +0300

    Δοκιμή

--- claims.py
+++
@@ -1,6 +0,0 @@
-# κανει εισαγωγη το did στον πινακα του claim
-
-def insert_claim(did, cursor, db):
-    if did:
-        cursor.execute("INSERT IGNORE INTO claims (did) VALUES (%s)", (did,))
-        db.commit()
--- classification.py
+++
@@ -1,6 +0,0 @@
-# κανει εισαγωγη το did στον πινακα του classification
-
-def insert_classification(did, cursor, db):
-    if did:
-        cursor.execute("INSERT IGNORE INTO classification (did) VALUES (%s)", (did,))
-        db.commit()
